5044016d572a0c12ed54ba101559fbbdc4d3ac40
tingwei <yuan097@kimo.com>
2020-11-09 13:57:59 +0800

    新增登入會員切換至相對role頁面

--- add_user.php
+++ add_user.php
@@ -2,4 +2,45 @@
 
 // 處理新增使用者功能
 
+$dsn="mysql:host=localhost;dbname=member;charset=utf8";
+$pdo=new PDO($dsn,'root','');
+
+$acc=$_POST['acc'];
+$password=$_POST['password'];
+$name=$_POST['name'];
+$birthday=$_POST['birthday'];
+$address=$_POST['address'];
+$tel=$_POST['tel'];
+$email=$_POST['email'];
+$education=$_POST['education'];
+
+$insert_to_login="insert into `login`(`acc`,`password`,`email`) values('$acc','$password','$email')";
+var_dump($insert_to_login);
+//查詢用query
+// $pdo->query($insert_to_login);
+$pdo->exec($insert_to_login);
+//新增修改 用exec
+
+$select_login_user="select * from `login` where `acc`='$acc' && `password`='$password'";
+$login_user=$pdo->query($select_login_user)->fetch();
+$login_id=$login_user['id'];
+echo "<br>最新註冊帳號<br>".$login_id;
+
+
+// 寫入使用者資料表
+
+$insert_to_member="insert into `member`(`name`,`birthday`,`role`,`address`,`tel`,`education`,`login_id`) values('$name','$birthday','會員','$address','$tel','$education','$login_id')";
+// $pdo->exec($insert_to_member);
+
+$result=$pdo->exec($insert_to_member);
+
+
+if($result){
+    header("location:index.php?meg=新增成功");
+}else{header("location:index.php?meg=新增失敗");}
+//exec 回傳值>1 等於新增成功
+//query回傳是物件 exec回傳數字
+
+echo '新增完成';
+
 ?>
--- check.php
+++ check.php
@@ -12,10 +12,65 @@ $dsn="mysql:host=localhost;dbname=member;charset=utf8";
 $pdo=new PDO($dsn,'root','');
 
 $acc=$_POST['acc'];
-$pw=$_POSTp['pw'];
+$password=$_POST['pw'];
 
-$sql="select * from `login` where `acc`='$acc' && `password`='$pw'";
+// 建立檢查帳號密碼的SQL語法
+$sql="select * from `login` where `acc`='$acc' && `password`='$password'";
+// 執行SQL語法並取得資料
 $check=$pdo->query($sql)->fetch();
+print_r($check);
+
+
+// $sql1="select count(*) from `login` where `acc`='$acc' && `password`='$password'";
+// $check1=$pdo->query($sql)->fetchColumn();
+// $sql作法較危險 有可能不小心把會員資料顯示出來 如果+count(*) 只會顯示數字(總共有幾筆資料)
+// fetch顯示完整陣列
+// fetch column顯示　使用者資料 一個一個欄位取出來
+print_r($check1);
+
+
+
+// 判斷回傳值是否為空
+if(!empty($check)){
+    echo '登入成功';
+    $member="select * from `member` where `login_id`='{$check['id']}'";
+}else{
+    header("location:index.php?meg=帳密不正確，請重新登入或註冊新帳號");
+}
+
+
+// 取得個人資料
+$member_sql="select * from `member` where `login_id`='{$check['id']}'";
+$member=$pdo->query($member_sql)->fetch();
+$role=$member['role'];
+
+switch($role){
+    case '會員':header('location:mem.php');
+break;
+    case 'VIP':header('location:vip.php');
+break;
+    case'管理員':header('location:admin.php');
+break;
+}
+
+
+
+
+
+
+
+
+
+
+
+// $db_acc=$check['acc'];
+// $db_px=$check['password'];
+
+// if($acc==$db_acc && $password==$db_pw){
+//     echo '帳密正確';
+// }else{echo '帳密不正確';}
+
+
 
 // 資料會是array格式 所以用print_r
 print_r($check);
